c8d210322ea9e8a62c1df2f6acf71ad966fddae6
Ben Thompson <benthompson@Bens-MacBook-Pro.local>
2018-06-14 07:40:21 -0500

    updating script

--- update_data.sh
+++ update_data.sh
@@ -1,23 +1,25 @@
+#Run this script from pi to avoid ssh + commands
+
 #ssh into pi
-ssh -p 1722 pi@shremore.chickenkiller.com
+#ssh -p 1722 pi@shredmore.chickenkiller.com
 
 #cd into depression in text
 cd depression_id_text
 
 #git pull origin master
-git pull origin master
+#git pull origin master
 
 #archive old data sets into archive foler
 DATE=`date +%Y-%m-%d`
 file_name='depression_tweets'
 file_ext='.csv'
-mv  $file_name$file_ext archive/$file_name$DATE$file_ext
+mv  $file_name$file_ext data_archive/$file_name$DATE$file_ext
 
 #concat all files into one a place in depression_id_text folder as depression_tweets.csv
 cat ~/cronjobs/twitter_data_depression/tweets/*.csv > $file_name$file_ext
 
 #git push origin master
-git push origin master
+#git push origin master
 
 #count number of lines in depression_tweets.csv save as variable
 
